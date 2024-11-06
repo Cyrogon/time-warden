533fcad4b450bae7c00d2464ab997aaa330fe095
jackbruce318 <128170835+jackbruce318@users.noreply.github.com>
2024-11-06 18:08:59 +0000

    Initial Class Creation
    Basic classes User, Shift, and Payslip have been created. Primitive versions of their cardinality are present and should be usable after database implementation. Controllers with Read/Write actions are in place however these cannot be fully implemented.

---
+++ time-warden/Models/Shift.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace time_warden.Models
+{
+    public class Shift
+    {
+        public int ShiftId { get; set; }
+
+        [Display(Name = "Clock In Time")]
+        public DateTime ClockInTime { get; set; }
+
+        [Display(Name = "Clock Out Time")]
+        public DateTime ClockOutTime { get; set; }
+
+        [Display(Name = "Hours Worked")]
+        public TimeSpan HoursWorked { get; set; }
+
+        [Display(Name = "Shift Worked?")]
+        public bool IsWorked { get; set; }
+
+        public DateTime ShiftDate {  get; set; }
+
+        //Navigational Properties - These are properties which are related to other classes and the basis of their relationships
+
+        public string UserId { get; set; }
+        public User User { get; set; }
+    }
+}
---
+++ time-warden/Models/User.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace time_warden.Models
+{
+    public class User:IdentityUser
+    {
+        // Extending IdentityUser with these properties
+        [Display(Name = "First Name")]
+        [Required]
+        public string FirstName { get; set; }
+
+        [Required]
+        public string Surname { get; set;}
+
+        [Required]
+        public string Address { get; set; }
+
+        [Required]
+        public string PostCode { get; set; }
+
+        [Display(Name = "Date Hired")]
+        [Required]
+        public DateTime DateHired { get; set; }
+
+        public string Department { get; set; }
+
+        [Display(Name = "Date of Birth")]
+        [Required]
+        public DateTime DateOfBirth { get; set; }
+
+        [Display(Name = "Phone Number")]
+        [Required]
+        public string Mobile {  get; set; }
+
+        [Display(Name = "Hourly Rate")]
+        [Required]
+        public decimal HourlyRate { get; set; }
+
+        //Navigational Properties - These are properties which are related to other classes and the basis of their relationships
+        public List<Shift> Shifts { get; set; }
+    }
+}
